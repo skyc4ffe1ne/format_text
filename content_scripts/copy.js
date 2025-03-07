f6763a76e6e5ddf29ed262a224a7e3998308fcfe
skyc4ffe1ne <skyc4ff3ine@virgilio.it>
2025-03-07 21:03:02 +0100

    Added list format,remove little bugs

--- content_scripts/copy.js
+++ content_scripts/copy.js
@@ -2,7 +2,7 @@ let finalText = [];
 
 const regexForCode = /<(table|tbody|tr|td)>/g;
 
-window.addEventListener("copy", function (e) {
+window.addEventListener("copy", function(e) {
   let selected = window.getSelection();
   let selectedText = selected.toString();
 
@@ -13,14 +13,14 @@ window.addEventListener("copy", function (e) {
     let fragment = range.cloneContents();
 
     let temp = document.createElement("div");
-    temp.appendChild(fragment);
-
     console.log("Temp: ", temp);
+    temp.appendChild(fragment);
 
     for (let i = 0; i < temp.childNodes.length; i++) {
       if (
         temp.childNodes[i].nodeName.toLowerCase() !== "span" &&
         temp.childNodes[i].nodeName.toLowerCase() !== "table" &&
+        temp.childNodes[i].nodeName.toLowerCase() !== "li" &&
         temp.childNodes[i].nodeName.toLowerCase() !== "#text"
       ) {
         takeRightNode(temp.childNodes[i]);
@@ -32,9 +32,8 @@ window.addEventListener("copy", function (e) {
 
   e.preventDefault();
 
-  console.log("Final text: ", finalText);
-
   let formattedText = finalText;
+  console.log("FinalText: ", finalText);
   finalText = [];
 
   e.clipboardData.setData("text/plain", formattedText.join(""));
@@ -42,41 +41,39 @@ window.addEventListener("copy", function (e) {
 
 function takeRightNode(temp) {
   for (let i = 0; i < temp.childNodes.length; i++) {
-    if (temp.childNodes[i].nodeName !== "#text") {
-      whatIsIt(
-        temp.childNodes[i],
-        temp.childNodes[i].classList,
-        temp.innerText,
-      );
-    } else {
-      finalText.push(temp.childNodes[i].textContent);
-    }
+    whatIsIt(temp.childNodes[i], temp.childNodes[i].classList, temp.innerHTML);
   }
 }
 
 function goodFormat(temp, tempType) {
+  const regexForNewLine = /\n(?!$)/g;
+  const tempWithoutN = temp.textContent.replace(regexForNewLine, "");
+  console.log("TempWitoutN: ", tempWithoutN);
   if (tempType === "code") {
-    let notFormatted = temp.textContent;
-    let formatted = "\n```java \n" + notFormatted + "\n```\n";
+    let formatted = "\n```java \n" + tempWithoutN + "\n```\n";
     finalText.push(formatted);
   } else if (tempType === "span") {
-    let notFormatted = temp.textContent;
-    let formatted = "**" + notFormatted + "**";
+    let formatted = "**" + tempWithoutN + "**";
+    finalText.push(formatted);
+  } else if (tempType === "list") {
+    let formatted = "- " + tempWithoutN + "\n";
     finalText.push(formatted);
   } else {
-    finalText.push(temp.textContent);
+    finalText.push(tempWithoutN);
   }
 }
 
-function whatIsIt(temp, tempClass = "", tempInner) {
+function whatIsIt(temp, tempClass, tempInner) {
   if (
+    tempInner.match(regexForCode) ||
     tempClass?.contains("SCodeFlow") ||
-    tempClass?.contains("JavaHighlightBlock") ||
-    tempInner.match(regexForCode)
+    tempClass?.contains("JavaHighlightBlock")
   ) {
     goodFormat(temp, "code");
   } else if (temp.nodeName.toLowerCase() === "span") {
     goodFormat(temp, "span");
+  } else if (temp.nodeName.toLowerCase() === "li") {
+    goodFormat(temp, "list");
   } else {
     goodFormat(temp, "text");
   }
